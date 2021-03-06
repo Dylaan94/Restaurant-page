421a39cb63fff31493ca10dfce24905a593b7ecc
Dylan Brain <dylanbrain@googlemail.com>
2021-03-06 18:53:44 +0900

    added divs to about page

--- README.md
+++ README.md
@@ -5,3 +5,9 @@ Things I've learnt
 - Remember to keep script tag at bottom of HTML
 - How to set up webpack for a simple project
 - Methods to keep styling responsive
+
+Things I need to keep learning
+- How to debug properly when using webpack
+
+Things I want to add 
+- Use google API to add maps to contact page
--- src/modules/aboutPage.js
+++ src/modules/aboutPage.js
@@ -3,9 +3,26 @@ let aboutPageTest = () => {
 }
 
 let loadAboutPage = () => {
+    let divArray = ['aboutTitle', 'aboutText',];
     let otherContent = document.getElementById('otherContent');
+    otherContent.innerHTML = "";
+
+    for (let i = 0; i < divArray.length; i++) {
+        let div = document.createElement('div');
+        div.className = divArray[i];
+        div.id = divArray[i];
+        otherContent.appendChild(div)
+    }
     console.log(otherContent)
-    otherContent.innerHTML = "About"
+    renderText();
+}
+
+let renderText = () => {
+    let aboutTitle = document.getElementById('aboutTitle')
+    let aboutText = document.getElementById('aboutText');
+
+    aboutTitle.innerHTML = "About Aso's Okonomiyaki"
+    aboutText.innerHTML = "Establisehd in 1998, Aso has been serving the people of Onomichi proudbly for over 20 years!"
 }
 
 export {aboutPageTest,
