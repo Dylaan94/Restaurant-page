11c7a4d7e9cee3a3b569dc96926637eeef0bebb2
Dylan Brain <dylanbrain@googlemail.com>
2021-03-08 20:57:39 +0900

    added menu items and styling

--- README.md
+++ README.md
@@ -6,6 +6,7 @@ Things I've learnt
 - How to set up webpack for a simple project
 - Methods to keep styling responsive
 - Removing the last element of a collection, is in general faster than removing the first element (see index.js)
+- create element only creates one item at a time. For example if adding multiple divs you have to create multiple createElements
 
 Things I need to keep learning
 - How to debug properly when using webpack
--- src/modules/menuPage.js
+++ src/modules/menuPage.js
@@ -1,3 +1,5 @@
+// need to clean up variables, make more readable.
+
 let menuPageTest = () => {
     console.log("Menu Page Running")
 }
@@ -5,6 +7,9 @@ let menuPageTest = () => {
 let loadMenuPage = () => {
     let divArray = ['menuTitle', 'menuContainer']
     let otherContent = document.getElementById('otherContent')
+
+    // dynamically create divs based off Array
+
     for (let i = 0; i < divArray.length; i++) {
         let div = document.createElement('div');
         div.id = divArray[i];
@@ -13,15 +18,150 @@ let loadMenuPage = () => {
     }
     console.log(otherContent);
 
-    renderMenuPage()
-}
-
-let renderMenuPage = () => {
     let menuTitle = document.getElementById('menuTitle')
     let menuContainer = document.getElementById('menuContainer')
 
+    // Add titles
     menuTitle.innerHTML = 'Menu'
-    menuContainer.innerHTML = 'Menu Container'
+
+    // dynamically create new divs for rendering menu
+
+    let menuItemsArr = ['noodles', 'toppings', 'extras']
+
+    for (let i = 0; i < menuItemsArr.length; i++) {
+        let div = document.createElement('div');
+        div.id = menuItemsArr[i];
+        div.className = menuItemsArr[i];
+        menuContainer.appendChild(div)
+    }
+
+    // create seperate divs for each menu section
+
+    let noodlesDiv = document.getElementById('noodles')
+    let toppingsDiv = document.getElementById('toppings')
+    let extrasDiv = document.getElementById('extras')
+
+    let menuContainerArr = ['Title','Type', 'Price']
+
+    for (let i = 0; i < menuContainerArr.length; i++) {
+
+        let div = document.createElement('div');
+        div.id = 'noodles' + menuContainerArr[i];
+        div.className = 'noodles' + menuContainerArr[i];
+
+        noodlesDiv.appendChild(div);
+
+        let div2 = document.createElement('div')
+        div2.id = 'toppings' + menuContainerArr[i];
+        div2.className = 'toppings' + menuContainerArr[i]
+
+        toppingsDiv.appendChild(div2)
+
+        let div3 = document.createElement('div')
+        div3.id = 'extras' + menuContainerArr[i];
+        div3.className = 'extras' + menuContainerArr[i]
+
+        extrasDiv.appendChild(div3)
+    }
+
+    // add titles to new divs
+
+    let noodlesTitle = document.getElementById('noodlesTitle').textContent = 'Noodles'
+    let toppingsTitle = document.getElementById('toppingsTitle').textContent = 'Toppings'
+    let extrasTitle = document.getElementById('extrasTitle').textContent = 'Extras'
+
+
+
+    console.log(menuContainer);
+
+   renderMenu(menuContainer, menuTitle)
+}
+
+let renderMenu = () => {
+
+    // add data to divs that were created above
+
+    let noodlesType = document.getElementById('noodlesType');
+    let noodlesPrice = document.getElementById('noodlesPrice');
+
+    let toppingsType = document.getElementById('toppingsType');
+    let toppingsPrice = document.getElementById('toppingsPrice');
+
+    let extrasType = document.getElementById('extrasType')
+    let extrasPrice = document.getElementById('extrasPrice')
+
+    console.log(menuContainer)
+    
+    // Menu Arrays to be dynamically added
+
+    let noodleArray = [
+                        {type: 'soba',
+                        price: "¥800"},
+                        {type: 'spicy soba',
+                        price: '¥850'},
+                        {type: 'udon',
+                        price: '¥800'}
+                    ]
+
+    let toppingArray = [
+                        {type: 'pork',
+                        price: '¥100'},
+                        {type: 'mochi',
+                        price: '¥150'},
+                        {type: 'cheese',
+                        price: '¥120'},
+                        {type: 'kimchi',
+                        price: '¥80'},
+                        {type: 'squid',
+                        price: '¥200'}
+                    ]
+
+    let extrasArray = [
+                        {type: 'beer',
+                        price: '¥500'},
+                        {type: 'cola',
+                        price: '¥200'},
+                        {type: 'water',
+                        price: '¥0'}
+                    ]
+
+    // dynamically add array items
+
+    //noodles
+    for (let i = 0; i < noodleArray.length; i++) {
+        let typeValue = document.createElement('p');
+        let priceValue = document.createElement('p')
+
+        typeValue.textContent = noodleArray[i].type;
+        noodlesType.appendChild(typeValue)
+
+        priceValue.textContent = noodleArray[i].price;
+        noodlesPrice.appendChild(priceValue)
+    }
+    // toppings
+    for (let i = 0; i < toppingArray.length; i++) {
+        let typeValue = document.createElement('p');
+        let priceValue = document.createElement('p')
+
+        typeValue.textContent = toppingArray[i].type;
+        toppingsType.appendChild(typeValue)
+
+        priceValue.textContent = toppingArray[i].price;
+        toppingsPrice.appendChild(priceValue)
+    }
+    // extras
+    for (let i = 0; i < extrasArray.length; i++) {
+        let typeValue = document.createElement('p');
+        let priceValue = document.createElement('p')
+
+        typeValue.textContent = noodleArray[i].type;
+        extrasType.appendChild(typeValue)
+
+        priceValue.textContent = noodleArray[i].price;
+        extrasPrice.appendChild(priceValue)
+    }
+
+  
 }
 export {menuPageTest,
         loadMenuPage}
